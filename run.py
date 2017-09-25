22f1eee7949d6d2d3cccc18160573bd0105095e3
ZhangXinNan <zhangxin19870504@163.com>
2017-09-26 07:10:55 +0800

    rename test.py run.py, add modify MANIFEST.in

--- run.py
+++ run.py
@@ -3,12 +3,11 @@ import argparse
 from PIL import Image
 import zbarlight
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--image', default='./tests/fixtures/two_qr_codes.png', help='input image')
-    return parser.parse_args()
 
-def main(args):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', default='./tests/fixtures/two_qr_codes.png', help='input image')
+    args = parser.parse_args()
     with open(args.image, 'rb') as image_file:
         image = Image.open(image_file)
         image.load()
@@ -16,4 +15,4 @@ def main(args):
         print('QR codes: %s' % codes)
 
 if __name__ == '__main__':
-    main(get_args())
+    main()
