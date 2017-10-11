7e2e27874e3829cd977054e0424da4d658b63f32
zhangxin <zhangxin19870504@163.com>
2017-10-11 10:26:45 -0500

    Add script to scan QR codes in an image file

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+import argparse
+from PIL import Image
+import zbarlight
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', help='input image')
+    args = parser.parse_args()
+    with open(args.image, 'rb') as image_file:
+        image = Image.open(image_file)
+        image.load()
+        codes = zbarlight.scan_codes('qrcode', image)
+        print('QR codes: %s' % codes)
+
+if __name__ == '__main__':
+    main()
