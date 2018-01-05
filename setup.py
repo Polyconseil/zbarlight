124fed1d3dcf2658a14e96861ffc803478edcb8a
Fabien Bochu <fabien.bochu@polyconseil.fr>
2018-01-05 16:54:04 +0100

    Switch to setup.cfg for metadata

--- setup.py
+++ setup.py
@@ -1,40 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os
-from setuptools import setup, find_packages, Extension
-
-
-def read(file_path):
-    with open(file_path) as fp:
-        return fp.read()
-
+from setuptools import setup, Extension
 
 setup(
-    name='zbarlight',
-    version='1.3.dev0',
-    description="A simple zbar wrapper",
-    long_description=read('README.rst'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: C',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-    ],
-    keywords=['zbar', 'QR code reader'],
-    author='Polyconseil',
-    author_email='opensource+zbarlight@polyconseil.fr',
-    url='https://github.com/Polyconseil/zbarlight/',
-    license='BSD',
-    packages=find_packages(where='src', exclude=['docs', 'tests']),
-    package_dir={'': str('src')},
+    setup_requires=['setuptools>=30.3'],
+    package_dir={  # FIXME: wait for https://github.com/pypa/setuptools/issues/1136
+        '': 'src',
+    },
     ext_modules=[
         Extension(
             name=str('zbarlight._zbarlight'),
@@ -43,13 +15,5 @@ setup(
             libraries=['zbar'],
             optional=os.environ.get('READTHEDOCS', False),  # Do not build on Read the Docs
         ),
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    setup_requires=[
-        'setuptools',
-    ],
-    install_requires=[
-        'Pillow',
     ],
 )
